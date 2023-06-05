d733785cbb5db4662e6b32cc34514ce2ef837ced
Manu-Abuya <manuabuya69@gmail.com>
2023-06-05 14:32:45 +0300

    Security group

--- aws-terraform-automation/main.tf
+++ aws-terraform-automation/main.tf
@@ -15,3 +15,30 @@ resource "aws_subnet" "my_subnet" {
     availability_zone       = "${var.aws_region}a"
 }
 
+resource "aws_security_group" "my_sg" {
+  name        = "my-security-group"
+  description = "Allow inbound SSH and HTTP traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
