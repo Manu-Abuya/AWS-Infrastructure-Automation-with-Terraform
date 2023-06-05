f6c477d3bd5242ea146d8c14898c5b99256fe5ff
Manu-Abuya <manuabuya69@gmail.com>
2023-06-05 14:32:00 +0300

    VPC and Subnet

--- aws-terraform-automation/main.tf
+++ aws-terraform-automation/main.tf
@@ -4,3 +4,14 @@
 provider "aws" {
     region = var.aws_region
 }
+
+resource "aws_vpc" "my_vpc" {
+    cidr_block = var.vpc_cidr_block
+}
+
+resource "aws_subnet" "my_subnet" {
+    vpc_id                  = aws_vpc.my_vpc.id
+    cidr_block              = "10.0.0.0/24"
+    availability_zone       = "${var.aws_region}a"
+}
+
